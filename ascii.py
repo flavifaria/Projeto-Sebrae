237ff58f861294d98e61291495ac019b7702ce53
jjjsssssf <joao.rosa.preto@escola.pr.gov.br>
2025-06-29 22:21:26 -0300

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,63 @@
+class art_ascii:
+    def __init__(self):
+        self.art1 = '''                .----.
+    .---------. | == | 
+    |.-"""""-.| |----| 
+    ||       || | == | 
+    ||       || |----| 
+    |'-.....-'| |::::| 
+    `"")---(""` |___.| 
+  /:::::::::::\" _  " 
+ /:::=======:::\`\`\ 
+`"""""""""""""`  '-' 
+'''
+
+        self.pc = f'''URGENTE!! Uma nova variante dos 
+mosquitos da dengue vem surgundo
+dos lugares mais remotos do Brasil
+Uma grande lago que foi poluido
+pelas grades corpoorações e popolação
+Você lendo isso nem se preocupa com
+isso e decide continuar sentado
+PRECIONE ENTER:'''
+        self.pc1= f'''Essa Variante de mosquitos são maiores
+mais fortes e estão se reproduzindo 
+mais rapido e cada dia estão mais 
+agrecivos e estão invadindo as cidades
+do Brasil todo e em alguns mês podem
+dominar o Brasil depois o mundo
+PRECIONE ENTER:'''
+        self.sair = '''  __                   ___       
+ |""|  ___    _   __  |"""|  __  
+ |""| |"""|  |"| |""| |"""| |""| 
+ |""| |"""|  |"| |""| |"""| |""| 
+ |""| |"""|  |"| |""| |"""| |""| 
+ """"""""""""""""""""""""""""""""" '''
+        
+        self.andar = '''
+Você decide sair um pouco de casa para 
+tentar sequeser os problemas de sua 
+vida e do mundo atual e suspira e 
+você vê um  desses tais mosquitos  
+geneticamente modificados atacando 
+uma senhoria e você deside agir'''
+        self.andar2 = '''Após derrotar o Mosquito a senhora
+te agradesse te dando uma pomada
+de regeneração para te ajudar 
+não sua missão que você agora 
+está sem mesmo nem ter persebido
+PRECIONE ENTER:'''
+        self.ops = '''
+Você Agradece e começa ver sua 
+cidade oque você vai fazer agora?
+Digito de Açôes do perssonagem
+[1]Andar [2]Menu [3]Sair'''
+        self.ops1 = '''
+Quer andar para qual região?:
+[1]Sul   [2]Norte 
+[3]Leste [4]Oeste    
+'''
+        self.ops2 = '''
+[1]Status   [2]Inventario
+[3]Melorias [4]Sair
+'''
